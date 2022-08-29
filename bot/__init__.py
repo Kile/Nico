c7c71decca966d7a2df94a1d3c5b5ca9cb7294b2
Kile <69253692+Kile@users.noreply.github.com>
2022-08-29 12:55:12 +0200

    Improve error display

--- bot/__init__.py
+++ bot/__init__.py
@@ -7,23 +7,13 @@ from . import cogs
 from .static.constants import TOKEN, ServerInfo, GUILD_OBJECT, ACTIVITY_EVENT, TRIALS
 from .utils.functions import is_dev
 
-import logging
-import logging.handlers
+import logging, sys
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.ERROR)
 logging.getLogger('discord.http').setLevel(logging.ERROR)
 
-handler = logging.handlers.RotatingFileHandler(
-    filename='discord.log',
-    encoding='utf-8',
-    maxBytes=32 * 1024 * 1024,  # 32 MiB
-    backupCount=5,  # Rotate through 5 files
-)
-dt_fmt = '%Y-%m-%d %H:%M:%S'
-formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='[%(asctime)s:%(levelname)s:%(name)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 class Bot(commands.Bot):
     def __init__(self, *args, **kwargs):
