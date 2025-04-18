1140a3b4daf1cc73b7fec26fae60d1342f3c5da0
Kile <k1ds1nth3dark@gmail.com>
2025-04-18 12:32:52 +0200

    Add tag rules

---
+++ bot/cogs/tag.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+from bot.__init__ import Bot
+
+from datetime import timedelta
+
+TAG_ROLE = 1362726731768922205
+
+class Tag(commands.Cog):
+    def __init__(self, client: Bot):
+        self.client = client
+
+    async def cog_load(self):
+        print("Loaded tag cog")
+
+    @commands.Cog.listener()
+    async def on_interaction(self, interaction: discord.Interaction):
+        if not interaction.data.get("custom_id") == "full_access":
+            return
+        
+        # If they have been in the server for less than 24 hrs, say no
+        if interaction.user.joined_at > discord.utils.utcnow() - timedelta(hours=24):
+            return await interaction.response.send_message("You need to be in the server for at least 24 hours to get full access.", ephemeral=True)
+        
+        # remove the tag role
+        if TAG_ROLE in [r.id for r in interaction.user.roles]:
+            await interaction.user.remove_roles(TAG_ROLE)
+            return await interaction.response.send_message("You have been removed from the tag role.", ephemeral=True)
+        
+    # Add tag role on join
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        if member.guild.id != self.client.server_info.ID: return # Don't want that process when someone is joining on another server
+
+        if member.bot: return
+
+        await member.add_roles(TAG_ROLE)
+
+Cog = Tag
